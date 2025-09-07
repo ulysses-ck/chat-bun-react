3a87bc021f28e21f7596e99c4a1454dacb17536f
Ulises <ulisesapaza.280@gmail.com>
2025-09-07 18:55:45 -0300

    fix: change body validation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,25 +4,26 @@ import { messageTable } from "./db/schema";
 
 const app = new Elysia()
   .ws("/chat", {
-    body: t.Array(
-      t.Object({
-        id: t.Optional(t.String()),
-        content: t.String(),
-        createdAt: t.Optional(t.String()),
-      })
-    ),
+    body: t.Object({
+      id: t.Optional(t.String()),
+      content: t.String(),
+      createdAt: t.Optional(t.String()),
+    }),
     async open(ws) {
       console.log(ws.id);
       const msg = `${ws.id} has entered to the chat`;
       const historialMessages = await db.query.messageTable.findMany();
       ws.subscribe("chat");
-      ws.send(historialMessages)
+      ws.send(historialMessages);
       ws.publish("chat", [
         { content: msg, id: ws.id, createdAt: new Date().toISOString() },
       ]);
     },
     async message(ws, body) {
-      const result = await db.insert(messageTable).values({content: body[0].content}).returning();
+      const result = await db
+        .insert(messageTable)
+        .values({ content: body.content })
+        .returning();
       ws.send(result);
       ws.publish("chat", result);
     },
